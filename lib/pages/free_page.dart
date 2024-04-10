55f3013c88aab8a5bc235115a8a57c4809fb644d
Esther Njihia <esthernjihia44@gmail.com>
2024-04-10 06:46:10 -0700

    Added a free tips page

---
+++ lib/pages/free_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FreePage extends StatelessWidget {
+  const FreePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("FREE PAGE"),
+    );
+  }
+}
